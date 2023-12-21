2d89a6ec4556343010891dd1e11714f3c74a8bc9
Rameshgandham <153059628+Rameshgandham@users.noreply.github.com>
2023-12-21 09:58:27 +0530

    Add files via upload
    commited first

---
+++ src/test/java/com/buildjob/Class1.java
@@ -0,0 +1,11 @@
+package com.buildjob;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my build is sucessfully completed");
+
+	}
+
+}
